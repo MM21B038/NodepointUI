0cff8e34c933addfb442f61315c66d8a6abfc47c
[dyad] <git@dyad.sh>
2025-10-22 19:30:41 +0530

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout"; // Import the new Layout component
+import Layout from "./components/Layout";
 import Index from "./pages/Index";
-import Documents from "./pages/Documents"; // Import new pages
+import Documents from "./pages/Documents";
 import KnowledgeBase from "./pages/KnowledgeBase";
 import Ask from "./pages/Ask";
 import NotFound from "./pages/NotFound";
@@ -15,18 +15,21 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/documents" element={<Layout><Documents /></Layout>} />
-          <Route path="/knowledge-base" element={<Layout><KnowledgeBase /></Layout>} />
-          <Route path="/ask" element={<Layout><Ask /></Layout>} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      {/* Wrap multiple children in a React Fragment */}
+      <>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/documents" element={<Layout><Documents /></Layout>} />
+            <Route path="/knowledge-base" element={<Layout><KnowledgeBase /></Layout>} />
+            <Route path="/ask" element={<Layout><Ask /></Layout>} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     </TooltipProvider>
   </QueryClientProvider>
 );
