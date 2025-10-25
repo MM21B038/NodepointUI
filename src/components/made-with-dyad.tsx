155deddce2a04dc567caa18c83ac4b15b61d132e
[dyad] <git@dyad.sh>
2025-10-25 12:22:15 +0530

    [dyad] Created the MadeWithDyad component and fixed the import error in Layout.tsx. - wrote 2 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,1 +1,11 @@
+"use client";
 
+import React from "react";
+
+export const MadeWithDyad: React.FC = () => {
+  return (
+    <footer className="w-full p-2 text-center text-xs text-muted-foreground border-t bg-background">
+      Made with Dyad
+    </footer>
+  );
+};
