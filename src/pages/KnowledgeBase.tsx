dd09fb2e66f9f7085d3acdccf9058eb46203900c
[dyad] <git@dyad.sh>
2025-10-26 11:31:05 +0530

    [dyad] Reverted the last change to KnowledgeBase.tsx. - wrote 1 file(s)

--- src/pages/KnowledgeBase.tsx
+++ src/pages/KnowledgeBase.tsx
@@ -243,8 +243,7 @@ const KnowledgeBase: React.FC = () => {
                       <p className="font-medium text-sm">{node.label}</p>
                       <p className="text-xs text-gray-500">{node.type}</p>
                     </div>
-                  </div>
-                ))}
+                  ))}
               </div>
             )}
           </CardContent>
