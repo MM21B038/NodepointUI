122fcdf6c486bd22f012399ac74a99ab3ca2a98c
[dyad] <git@dyad.sh>
2025-11-29 23:23:54 +0530

    Reverted all changes back to version cb86c46e55d67fb6c6ec1782757623575303906b

--- src/components/SourceFilesPanel.tsx
+++ src/components/SourceFilesPanel.tsx
@@ -88,7 +88,7 @@ const SourceFilesPanel: React.FC<SourceFilesPanelProps> = ({
             Clear All
           </Button>
         </div>
-        <ScrollArea className="flex-grow pr-4 no-scrollbar">
+        <ScrollArea className="flex-grow pr-4">
           <div className="grid gap-2">
             {uniqueSourceFiles.map((file) => (
               <div key={file} className="flex items-center space-x-2">
