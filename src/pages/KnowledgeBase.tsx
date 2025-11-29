f34da4926c8f2fcd99f3ee8672a7dc326d3cf7bc
[dyad] <git@dyad.sh>
2025-11-29 23:23:28 +0530

    [dyad] wrote 4 file(s)

--- src/pages/KnowledgeBase.tsx
+++ src/pages/KnowledgeBase.tsx
@@ -474,7 +474,7 @@ const KnowledgeBase = () => {
                     <PanelRightClose className="h-4 w-4" />
                   </Button>
                 </h3>
-                <ScrollArea className="h-[calc(100%-57px)]">
+                <ScrollArea className="h-[calc(100%-57px)] no-scrollbar">
                   <DetailPanel item={selectedItem} />
                 </ScrollArea>
               </div>
